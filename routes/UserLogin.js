40d50ad4fb2601c8c6d051fed9ba2dfe876ec5ac
Vivek Vinze <vivek.vinze@gmail.com>
2020-07-26 16:52:05 +0530

    Login page new

--- routes/UserLogin.js
+++ routes/UserLogin.js
@@ -12,7 +12,7 @@ module.exports = function(app, passport) {
   app.get('/login', function(req, res) {
 
     // render the page and pass in any flash data if it exists
-    res.render('login.ejs', {
+    res.render('login-new.ejs', {
       message: req.flash('loginMessage')
     });
   });
