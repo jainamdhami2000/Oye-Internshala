9d58c284f2c6373e56590fdcfb8e126aa3dcdd1d
jainam <jainamdhami2000@gmail.com>
2020-09-01 00:08:09 +0530

    /jobapplications removed is__accept:true

--- routes/internship.js
+++ routes/internship.js
@@ -373,7 +373,6 @@ router.post('/jobapplications', isLoggedIn, (req, res) => {
   var job_id = req.body.job_id;
   Applicant.find({
     job_id: job_id,
-    is_accept: false
   }, (err, applicants) => {
     res.render('studentapplications', {
       user: req.user,
