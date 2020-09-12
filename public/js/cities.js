9e1fcbe2bc24eb93cfbb7429e6378949907e89ed
Vivek Vinze <vivek.vinze@gmail.com>
2020-09-13 00:47:37 +0530

    postintern.ejs

--- public/js/cities.js
+++ public/js/cities.js
@@ -2,7 +2,7 @@
 
 var loc = ['Delhi', 'Mumbai', 'Bangalore', 'Chennai', 'Hyderabad', 'Kolkata'];
 loc.forEach((cities) => {
-  $('#loc1').append('<option value="' + cities + '">' + cities + '</option>');
+  $('#loc').append('<option value="' + cities + '">' + cities + '</option>');
 });
 loc.forEach((cities) => {
   if (locationc == cities) {
